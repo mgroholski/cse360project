ce9b0c26fadb033ec361cadcc73bb1c46f7a9d7b
Matthew Groholski <mgrohols@asu.edu>
2022-11-13 23:15:14 -0700

    Final Final

--- src/main/java/com/squad/project/TabApplication.java
+++ src/main/java/com/squad/project/TabApplication.java
@@ -34,7 +34,7 @@ public class TabApplication extends Application{
     public void start(Stage primaryStage) throws Exception {
         Scene scene = new Scene(rootNode);
         primaryStage.setTitle("Welcome to SunDevil Pizza");
-        primaryStage.setHeight(2000);
+        primaryStage.setHeight(1000);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
